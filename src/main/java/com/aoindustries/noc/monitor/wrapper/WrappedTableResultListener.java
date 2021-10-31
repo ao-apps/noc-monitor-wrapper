4abf8740de8d1e130d7333e3dab9767d8a97f60c
AO Industries, Inc <support@aoindustries.com>
2021-10-30 23:56:44 -0500

    Changed links to https for https://www.gnu.org/licenses/

--- src/main/java/com/aoindustries/noc/monitor/wrapper/WrappedTableResultListener.java
+++ src/main/java/com/aoindustries/noc/monitor/wrapper/WrappedTableResultListener.java
@@ -18,7 +18,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with noc-monitor-wrapper.  If not, see <http://www.gnu.org/licenses/>.
+ * along with noc-monitor-wrapper.  If not, see <https://www.gnu.org/licenses/>.
  */
 package com.aoindustries.noc.monitor.wrapper;
 
